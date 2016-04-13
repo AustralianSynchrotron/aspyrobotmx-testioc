351aff83584dba65d59315fdfec3cf09470bdeeb
RobbieClarken <robbie.clarken@gmail.com>
2016-04-13 21:19:46 +1000

    Add robot PV database files

--- robotmxtestioc/iocBoot/iocrobotmx/st.cmd
+++ robotmxtestioc/iocBoot/iocrobotmx/st.cmd
@@ -7,10 +7,13 @@
 
 ## Register all support components
 dbLoadDatabase("../../dbd/robotmx.dbd",0,0)
-robotmx_registerRecordDeviceDriver(pdbbase) 
+robotmx_registerRecordDeviceDriver(pdbbase)
+
+epicsEnvSet(ROBOT,"ROBOT_MX_TEST")
 
 ## Load record instances
-dbLoadRecords("../../db/robotmx.db","user=root")
+dbLoadRecords("../../robotmxApp/Db/RobotEpsonIP.db","P=${ROBOT},R=,L0=0,L1=1, FLNK=${ROBOT}:GRIPOPEN_STATUS")
+dbLoadRecords("../../robotmxApp/Db/RobotEpsonMX.db","P=${ROBOT},R=,L0=0,L1=1, SCAN=.5 second")
 
 iocInit()
 
